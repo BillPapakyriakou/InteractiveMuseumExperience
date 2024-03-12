acc117c71128f0a357934ca630be669450a2a9e3
Bill Papakyriakou <bill.papakyriakou1999@gmail.com>
2024-03-12 04:00:54 +0200

    Added Soundtrack
    Music and Master volume sliders
    Added Menu soundtrack
    Added Game soundtrack
    -Thanks Gran Turismo

--- Interactive Museum Experience/Assets/Scripts/MainMenu.cs
+++ Interactive Museum Experience/Assets/Scripts/MainMenu.cs
@@ -2,9 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Slider[] volumeSliders;
+
+    void Start()
+    {
+        //volumeSliders = new Slider[1];
+
+        volumeSliders[0].value = AudioManager.instance.masterVolumePercent;
+        volumeSliders[1].value = AudioManager.instance.musicVolumePercent;
+
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   // load next scene in queue
@@ -14,5 +26,14 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quit Game");
         Application.Quit();   // works only on final build
+    }
+    public void SetMasterVolume(float value)
+    {
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Music);
     }
 }
