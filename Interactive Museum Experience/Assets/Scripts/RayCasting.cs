0a627672672ecaef7907684c225683f0dd0bc535
Bill Papakyriakou <bill.papakyriakou1999@gmail.com>
2024-03-14 16:48:04 +0200

    Item recognision (Beta)
    - Implemented item recognision with
      raycasts. Still not fully working,
      will fix later

---
+++ Interactive Museum Experience/Assets/Scripts/RayCasting.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCasting : MonoBehaviour
+{
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, 100))
+        {
+            //Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+            Debug.Log("We hit a " + hitInfo.transform.name);
+        }
+        else
+        {
+            //Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green);
+            
+        }
+    }
+
+    
+}
