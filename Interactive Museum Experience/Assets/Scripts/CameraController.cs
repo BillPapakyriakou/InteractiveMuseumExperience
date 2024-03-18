e51cd1423566f1707149d02de4dc43f98ce33061
Bill Papakyriakou <bill.papakyriakou1999@gmail.com>
2024-03-18 23:28:11 +0200

    Added some exhibits
    - Bust of Demosthenes 3D model
    - Victory of Samothrace 3D model
    - Did some adjustments to the player
       script
    - Slightly modified museum model

--- Interactive Museum Experience/Assets/Scripts/CameraController.cs
+++ Interactive Museum Experience/Assets/Scripts/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 20f;
 
     public Transform playerBody;
 
